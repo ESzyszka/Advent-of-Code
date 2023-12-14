d0beef018d6438e4b5dfc523e726bc4435a55deb
| Ewa Szyszka | エーワー シズカ | 에바 스스카 | <ea.szyszka@gmail.com>
2023-12-14 14:11:27 +0900

    Update day_5_.js

--- day5/day_5_.js
+++ day5/day_5_.js
@@ -47,10 +47,29 @@ fs.readFile(filePath, 'utf8', (err, data) => {
      //Spliting data based on empty line
     const input = data.split("\n\n");
 
-    //Extracting the data from input.txt
+    //**************************  Extracting the data from input.txt  **************************************
     const seeds = input[0].split(" ").map((x) => parseInt(x.trim())).filter(x => !isNaN(x));
 
+    /*
+    INPUT[1]:
+    seed-to-soil map:
+    50 98 2
+    52 50 48 
 
-    console.log(input[0], seeds);
+    OUTPUT:
+    [ '50 98 2', '52 50 48' ]
+    */
+
+    let [text, ...seedToSoil] = input[1].split("\n"); // ...seedToSoil --> this means the rest of the code
+    let[, ...soilToFertilizer] = input[2].split("\n");
+    let[, ...fertilizerToWater] = input[3].split("\n");
+    let[, ...waterToLight] = input[4].split("\n");
+    let[, ...lightToTemperature] = input[5].split("\n");
+    let[, ...temperatureToHumidity] = input[5].split("\n");
+    let[, ...humidityToLocation] = input[5].split("\n");
+
+    //Logging sample inputs 
+    console.log(input[3], fertilizerToWater);
+    //********************************************************************************************************
 
 });

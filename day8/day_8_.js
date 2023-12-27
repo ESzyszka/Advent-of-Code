0497269c8cbfd033487e7b221cc7a2bee868a5be
| Ewa Szyszka | エーワー シズカ | 에바 스스카 | <ea.szyszka@gmail.com>
2023-12-27 22:26:56 +0100

    day 8

--- day8/day_8_.js
+++ day8/day_8_.js
@@ -1,10 +1,59 @@
 //**************  Solutions for day 8  **************
 /*
-32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483
-ddddd ddd
-９9999 999
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
 */
+
+//******************  Each line of input.txt  ******************
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf8").split("\n");
+input.pop()
+
+const LR = String(input[0])
+
+console.log(typeof LR)
+//**************************************************************
+
+
+//**************  nullish coalescing operator  **************
+//LR = LR ?? 'LLRRRLLRLRRRLLRLRLRLRLRRRLRRLRRLRLLLRRLLRRLRRLRRLRRRLLLRRLRLRRRLRRRLRLRRLRRRLRLRRRLRLRLLLRLRRLRLRRLRRRLRLRRRLRRRLRRRLRRRLRLRRRLRRRLRLLRRLRLRLRRRLRRLRRRLRRRLRRRLRRRLLLLRRLLRLRRLRRLRRRLRRRLLLRRLRRLRLRRLRRRLRRLRLRRRLRLRRLLRLLRRLRLRRRLRRLRRLRLRRLLLRRRLRLRRRLRLRLLRLRLRRRLRLRLRRRLRRLRRLRRRLRRLLRRRR';
+
+
+// Given data
+const data = [
+    ['VTM', ['VPB', 'NKT']],
+    ['LHN', ['DLF', 'GQV']],
+    ['CRN', ['TGB', 'XCM']],
+    ['TXR', ['JQL', 'JQL']],
+    ['LJX', ['SPR', 'LLM']],
+    ['HSH', ['SKP', 'MFC']],
+    ['JSC', ['QPB', 'VCP']],
+    ['KKG', ['BFT', 'NBS']],
+    ['BCB', ['GRT', 'TLB']],
+    ['GPA', ['SDK', 'MRM']],
+    ['CNJ', ['FDR', 'GGT']],
+    ['GQV', ['BSJ', 'DGF']],
+    ['CNT', ['FLM', 'TVQ']],
+    ['RLL', ['BRB', 'CRH']],
+    ['PPN', ['KCF', 'PSJ']],
+    ['TFL', ['GCD', 'DMF']],
+    ['KPM', ['TKS', 'CGP']],
+    ['FBN', ['GPF', 'LCJ']]
+  ];
+  
+  // Convert to hash table
+  const hashTable = {};
+  data.forEach(([key, values]) => {
+    hashTable[key] = values;
+  });
+  
+  // Display the hash table
+  console.log(hashTable["VTM"]);
+  

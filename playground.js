97131fe37fcdfb09de7e4aadef89bee0bb76f1d5
| Ewa Szyszka | エーワー シズカ | 에바 스스카 | <ea.szyszka@gmail.com>
2024-01-04 23:42:33 +0100

    Update playground.js

--- playground.js
+++ playground.js
@@ -7,6 +7,11 @@ function multiplier(number){
     return number *2
 }
 
+//The same that is above can be in this case done with .map()
+const input_2 = [1,2,3,4,5];
+const new_2 = input_2.map(i => i*2)
+console.log("Map function input: " + input_2 + "Map function output* " + new_2)
+
 //**********  setTimeout()  **********
 //The function passed inside the setTimeout does not have ()
 //1000 = 1 second
@@ -65,6 +70,29 @@ capitals.push("Yerevan")
 capitals.push("Ouagadougou");
 console.log("This is the stack: " + JSON.stringify(capitals.items));
 
+//*********************  Functions  *********************
+/*
+The main difference between Function declaration and Function expression is we can invoke the function add(2,3) before its declaration also, 
+but the function addition(2,3) needs to invoke after it is defined.
+*/
+
+//****** Function declaration ******
+function add(a,b){
+    return a+b;
+}
+
+//Function expression
+const addition = function(a,b){
+    return a+b;
+}
+
+//Arrow function expression
+const arrow_add = (a,b) =>{
+    return a+b;
+}
+
+//Arrow function expression --> shorter
+const arrow_add_short = (a,b) => a+b;
 
 //**********  Object Oriented Programming  ****************
 
@@ -81,18 +109,3 @@ const person_one = new Person("경은", "김", 23);
 const person_two = new Person("Lu", "Ga", 60);
 
 document.getElementById("demo").innerHTML = "第一人の年齢: " + person_one.age + ". 第二人の年齢:  " + person_two.age + "."; 
-
-//*********************  Functions  *********************
-/*
-The main difference between Function declaration and Function expression is we can invoke the function add(2,3) before its declaration also, 
-but the function addition(2,3) needs to invoke after it is defined.
-
-*/
-//Function declaration
-function add(a,b){
-    return a+b;
-}
-//Function expression
-const addition = function(a,b){
-    return a+b;
-}
